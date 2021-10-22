a8056966bcb286460801362e431f5ad05976d116
OmarJ <omarjandali93@gmail.com>
2021-10-22 14:08:06 -0700

    create the initial attributes and methods for bank account

---
+++ BankAccount.py
@@ -0,0 +1,25 @@
+class BankAccount(self):
+  def __init__(self, full_name, account_number, account_balance, account_type):
+    self.full_name = full_name 
+    self.account_number = account_number
+    self.balance = account_balance
+    self.type = account_type
+
+  def deposit(self):
+    pass
+
+  def withdrawl(self):
+    pass
+
+  def get_balance(self):
+    pass 
+
+  def add_interest(self):
+    pass 
+
+  def print_statement(self):
+    pass
+
+john_doe_account = BankAccount()
+omar_jandali_account = BankAccount()
+mitchel_account = BankAccount()
